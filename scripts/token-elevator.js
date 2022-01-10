7205f71bd7580cce104aec2f9a90917bf1e588f1
Weissrolf <9654228-Weissrolf@users.noreply.gitlab.com>
2022-01-10 01:05:43 +0000

    Upload New File

---
+++ scripts/token-elevator.js
@@ -0,0 +1,58 @@
+var hoveredToken  // in module scope for checking which token is hovered on
+
+// changes token elevation or resets to 0, prefer hovered on tokens over selected
+async function setElevation (value) {
+  const tokens = hoveredToken ?? canvas.tokens.controlled // hovered or selected?
+  const updates = tokens.map(function (token) {
+      return ({
+        _id: token.id,
+        elevation: value === 0 ? 0 : token.data.elevation + value
+      })
+    })
+  await canvas.scene.updateEmbeddedDocuments("Token", updates)
+  // force tokenHUD re-render to update its elevation input dialog on key changes
+  if (canvas.hud.token.rendered) canvas.hud.token.render()
+}
+
+// set up default module keybindings
+Hooks.on("init", () => {
+  const unipreKey = "IntlBackslash" // default single key for everything
+  const preKeys = [
+    { change: -10, preKeys: unipreKey, keyMod: ["Control", "Shift"] },
+    { change: -5, preKeys: unipreKey, keyMod: ["Control"] },
+    { change: 0, preKeys: unipreKey, keyMod: ["Alt", "Shift"] },
+    { change: 5, preKeys: unipreKey, keyMod: [] },
+    { change: 10, preKeys: unipreKey, keyMod: ["Shift"] }
+  ]
+  // set up all the above keybindings in one loop
+  for (let key of preKeys) {
+    game.keybindings.register("token-elevator", `te${key.change}`, {
+      name: `Change token elevation by ${key.change}`,
+      hint: "Change token elevation of hovered on or selected tokens.",
+      editable: [{ key: key.preKeys, modifiers: key.keyMod }],
+      onDown: function () {
+        setElevation(key.change) // call to change token elevation
+      }
+    })
+  }
+})
+
+// query if token is hovered on
+Hooks.on("hoverToken", function (token, hovered) {
+  hoveredToken = hovered ? [token] : null
+})
+
+// allow mousewheel to change value of tokenHUD elevation box
+Hooks.on("renderTokenHUD", () => {
+  $(".elevation").bind("wheel", (catchEvent) => {
+    setElevation(
+      catchEvent.originalEvent.wheelDelta > 0
+        ? event.shiftKey // event. deprecated?
+          ? 10
+          : 5
+        : event.shiftKey // event. depredated?
+          ? -10
+          : -5
+    )
+  return catchEvent})
+})
