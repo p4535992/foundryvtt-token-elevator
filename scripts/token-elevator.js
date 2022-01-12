ed00490d488a3e1d85793aa1be7aa64d667a31bf
Weissrolf <timur.born@gmail.com>
2022-01-12 16:49:09 +0100

    formatting changes

--- scripts/token-elevator.js
+++ scripts/token-elevator.js
@@ -3,14 +3,11 @@ let hoveredTokens; // In module scope for checking which token is hovered on
 // Change token elevation or reset to 0, prefer hovered tokens over selected
 async function setElevation(value) {
   const tokens = hoveredTokens ?? canvas.tokens.controlled; // hovered or selected?
-  const updates = tokens.map(function (token) {
-    return {
-      _id: token.id,
-      elevation: value === 0 ? 0 : token.data.elevation + value,
-    };
-  });
+  const updates = tokens.map((token) => ({
+    _id: token.id,
+    elevation: value === 0 ? 0 : token.data.elevation + value,
+  }));
   await canvas.scene.updateEmbeddedDocuments("Token", updates);
-
   // Force token HUD to re-render, so that its elevation input shows the new height
   if (canvas.hud.token.rendered) {
     canvas.hud.token.render();
@@ -27,7 +24,6 @@ Hooks.on("init", () => {
     { change: 5, preKeys: unipreKey, keyMod: [] },
     { change: 10, preKeys: unipreKey, keyMod: ["Shift"] },
   ];
-
   // Set up all the above keybindings in one loop
   for (const key of preKeys) {
     game.keybindings.register("token-elevator", `te${key.change}`, {
@@ -52,7 +48,6 @@ Hooks.on("renderTokenHUD", () => {
     const originalEvent = catchEvent.originalEvent;
     const value = originalEvent.shiftKey ? 10 : 5;
     const sign = originalEvent.wheelDelta > 0 ? 1 : -1;
-
     setElevation(value * sign);
   });
 });
